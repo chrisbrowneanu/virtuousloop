c5fcab29ba20a15054bd08cde67f21e339b197f1
JennySimmons <51806325+JennySimmons@users.noreply.github.com>
2025-05-23 08:52:48 +1000

    Update analysis_many_eyes.py
    try self_df and shadow_df

--- scripts/analysis_many_eyes.py
+++ scripts/analysis_many_eyes.py
@@ -691,8 +691,8 @@ def analysis_many_eyes():
         this_crit_score=self_crit_score + shadow_crit_score
         students.loc[i,'text_score'] = this_text_score
         students.loc[i,'crit_score'] = this_crit_score
-        #students.loc[i,'self_wc'] = this_self_df['wc']
-        #students.loc[i,'shadow_wc'] = this_shadow_df['wc']
+        #students.loc[i,'self_wc'] = self_df['wc']
+        #students.loc[i,'shadow_wc'] = shadow_df['wc']
 
     
     students['text_rank']=students.text_score.rank(pct=True)
@@ -709,7 +709,7 @@ def analysis_many_eyes():
 
 
     # this_out=students[['user','secret','text_score', 'crit_score', 'score_rank']]
-    this_out=students[['user','firstname','lastname','group','text_score','crit_score', 'score_rank']]
+    this_out=students[['user','firstname','lastname','group','self_wc,'shadow_wc','text_score','crit_score', 'score_rank']]
     this_out.to_csv(c.f['wattle_analysis'], index=False)
 
 
